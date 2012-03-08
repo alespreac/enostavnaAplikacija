da160ecb46676b6b33e5df193248efdfd46b3cba
Ales Preac <ales.preac@gmail.com>
2012-03-08 12:54:14 +0100

    Title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  
+  #return a title on a per-page basic
+  def title
+    base_title = "Ruby on Rails Tutoria"
+    if @title.nil?
+      base_title
+    else
+      "#(base_title) | #{@title}"
+    end
+    
+  end
 end
