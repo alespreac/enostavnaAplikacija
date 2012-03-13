7b642e056ec0c25ab59e144126ebed98ad459b76
Ales Preac <ales.preac@gmail.com>
2012-03-13 11:49:23 +0100

    Add a stub User show page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessible :name, :email #preprecis uporabnikom dostop do 
+  
+  email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  
+  validates :name, :presence => true, #preprecis da bi bilo ime prazno
+                   :length   => { :maximum => 50 }
+  validate :email, :presence => true,
+                   :format => { :with => email_regex},
+                   :uniqueness => true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,15 @@
 EnostavnaAplikacija::Application.routes.draw do
   
+  resources :users
   
-  get "users/new"
+  
 
   root :to => "pages#home"
  
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
-  match 'signup', :to => 'users#new'
+  match '/signup', :to => 'users#new'
   
 
   # The priority is based upon order of creation:
