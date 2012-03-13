184c6f9600aaababc90c6239e7fb182115b06392
Ales Preac <ales.preac@gmail.com>
2012-03-13 13:32:05 +0100

    Add password and enycreped_password attributes to the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email #preprecis uporabnikom dostop do 
+  attr_accessor :password
+  
+  attr_accessible :name, :email, :password, :password_confirmation #preprecis uporabnikom dostop do 
   
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   validates :name, :presence => true, #preprecis da bi bilo ime prazno
                    :length   => { :maximum => 50 }
-  validate :email, :presence => true,
+  validates :email, :presence => true,
                    :format => { :with => email_regex},
                    :uniqueness => true
+  
+  validates :password, :presence => true,
+                       :confirmation => true,
+                       :length => {:within => 6..40} #velikost gelsa (med 6 in 40 znaki)
+  
+  
 end
